1447cd63a66020b2930ca1e5f842470080e39a2f
莫载斌 <zaibin_mo@sui.com>
2020-07-03 15:49:24 +0800

    update dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/convert',
-    name: 'convert2request',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('@/views/convert-to-request')
-  },
-  {
-    path: '/icon',
-    name: 'icon',
-    component: () => import('@/views/icons')
-  }
+const devRouters = [
+    {
+        path: '/icon',
+        name: 'icon',
+        component: () => import('@/views/icons')
+    }
+]
+
+const routes = [
+    {
+        path: '/',
+        name: 'convert2request',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/convert-to-request')
+    },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    // mode: 'history',
+    base: process.env.BASE_URL,
+    routes: process.env.NODE_ENV === 'development' ? [...devRouters, ...routes] : routes
 });
 
 export default router
